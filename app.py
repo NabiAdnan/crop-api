7e5148cc2c128b26c68320a6649769a3b43fb5fc
Mohammed Nabi Adnan Ahmed <nabiadnan.md@gmail.com>
2025-05-25 17:21:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
 import joblib
 import numpy as np
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Load model and scaler
 model = joblib.load("crop_model.pkl")
