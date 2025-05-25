24d1dc3d084c8a142a612d312e1d2dd21233e629
Mohammed Nabi Adnan Ahmed <nabiadnan.md@gmail.com>
2025-05-25 16:57:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,18 @@ def predict():
         # Make prediction
         prediction = model.predict(scaled_features)
 
-        # Convert to native Python string for JSON response
-        return jsonify({"recommended_crop": str(prediction[0])})
+
+        label_map = {
+        1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya",
+        7: "Orange", 8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes",
+        12: "Mango", 13: "Banana", 14: "Pomegranate", 15: "Lentil", 16: "Blackgram",
+        17: "Mungbean", 18: "Mothbeans", 19: "Pigeonpeas", 20: "Kidneybeans",
+        21: "Chickpea", 22: "Coffee"
+        }
+        
+        recommended_crop = label_map.get(int(prediction[0]), "unknown")
+        return jsonify({"recommended_crop": recommended_crop})
+
 
     except Exception as e:
         return jsonify({"error": str(e)}), 400
